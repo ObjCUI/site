fcb0da94c7a6b311eb459682e287fab2003c30d8
stephenwzl <wangzhilong.stephen@bytedance.com>
2020-04-10 13:46:48 +0800

    test

---
+++ docs/precache-manifest.26925a2ecd7f3e732e39ad12b6ca2b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263e4524e2597a7954ee1d67ef41ad5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b4eae770496fe883336",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01ae9d4b6eb5199a5d35",
+    "url": "./static/js/2.b9bc1e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b9bc1e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4eae770496fe883336",
+    "url": "./static/js/main.555774ff.chunk.js"
+  },
+  {
+    "revision": "a058aa7ebab77292f4db",
+    "url": "./static/js/runtime-main.f1612ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
